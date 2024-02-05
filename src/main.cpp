d0259dc7f1aea05824aa3b2e1658a62be37c9f65
Fujiii Shoki <2021d36@stu.gifu-nct.ac.jp>
2024-02-05 13:47:40 +0900

    パンナコット

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
 #include <Arduino.h>
+#include <avr/interrupt.h>
 //2つのデジタル信号と1つのアナログ信号を受け取りそれらを構造体に格納するプログラムを作成
 typedef struct{
     int digital_in1;
     int digital_in2;
     int analog_in;
 }input;
+
+input in;
+void read() {
+  in.digital_in2 = digitalRead(3);
+  in.analog_in = analogRead(A0);
+}
+
+ISR(INT0_vect) {
+  EIMSK = 0b00000000;
+  in.digital_in1 = digitalRead(2);
+  EIMSK = 0b00000001;
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,9 +26,14 @@ void setup() {
   pinMode(A0, INPUT);
   Serial.begin(9600);
 }
-
-void read(input in) {
-  in.digital_in1 = digitalRead(2);
-  in.digital_in2 = digitalRead(3);
-  in.analog_in = analogRead(A0);
-}
+ void loop() {
+  // put your main code here, to run repeatedly:
+  read();
+  Serial.print("digital_in1: ");
+  Serial.print(in.digital_in1);
+  Serial.print(" digital_in2: ");
+  Serial.print(in.digital_in2);
+  Serial.print(" analog_in: ");
+  Serial.println(in.analog_in);
+  delay(1000);
+ }

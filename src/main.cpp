c5e6974196124e397d4929acca26906113864ffd
Fujiii Shoki <2021d36@stu.gifu-nct.ac.jp>
2024-02-05 13:01:20 +0900

    結果にコミット

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,27 @@
 #include <Arduino.h>
+//2つのデジタル信号と1つのアナログ信号を受け取りそれらを構造体に格納するプログラムを作成
+typedef struct{
+    int digital_in1;
+    int digital_in2;
+    int analog_in;
+}input;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  input in;
+  in.digital_in1 = digitalRead(2);
+  in.digital_in2 = digitalRead(3);
+  in.analog_in = analogRead(A0);
+  Serial.print("digital_in1: ");
+  Serial.print(in.digital_in1);
+  Serial.print(" digital_in2: ");
+  Serial.print(in.digital_in2);
+  Serial.print(" analog_in: ");
+  Serial.println(in.analog_in);
+  delay(1000);
+}

4034e676dd80ff7ba807ffb4b5c9255f3612f00f
Fujiii Shoki <2021d36@stu.gifu-nct.ac.jp>
2024-02-05 14:23:37 +0900

    ゴミ

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #include <avr/interrupt.h>
 //2つのデジタル信号と1つのアナログ信号を受け取りそれらを構造体に格納するプログラムを作成
+int digital_in1;
 typedef struct{
-    int digital_in1;
     int digital_in2;
     int analog_in;
 }input;
@@ -14,7 +14,7 @@ void read(input *in) {
 
 ISR(INT0_vect) {
   EIMSK = 0b00000000;
-  
+  digital_in1 = 1;
   EIMSK = 0b00000001;
 }
 
@@ -27,6 +27,7 @@ void setup() {
 }
  void loop() {
   // put your main code here, to run repeatedly:
+  digital_in1 = 0;
   input *value;
   read(value);
  }

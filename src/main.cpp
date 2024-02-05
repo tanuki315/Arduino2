45aca9a2502b617981f64a7ba15af8d08d355180
Fujiii Shoki <2021d36@stu.gifu-nct.ac.jp>
2024-02-05 13:09:09 +0900

    関数化

--- src/main.cpp
+++ src/main.cpp
@@ -14,17 +14,8 @@ void setup() {
   Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  input in;
+void read(input in) {
   in.digital_in1 = digitalRead(2);
   in.digital_in2 = digitalRead(3);
   in.analog_in = analogRead(A0);
-  Serial.print("digital_in1: ");
-  Serial.print(in.digital_in1);
-  Serial.print(" digital_in2: ");
-  Serial.print(in.digital_in2);
-  Serial.print(" analog_in: ");
-  Serial.println(in.analog_in);
-  delay(1000);
 }
